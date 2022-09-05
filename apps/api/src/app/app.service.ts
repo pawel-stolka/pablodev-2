99f3bab65999e751717f59e048802baf8a8afe63
Pablo <pawel.stolka@gmail.com>
2022-09-05 21:36:51 +0200

    default endpoint

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -4,6 +4,6 @@ import { Message } from '@pablodev2/api-interfaces';
 @Injectable()
 export class AppService {
   getData(): Message {
-    return { message: 'Welcome to api!' };
+    return { message: 'Welcome to pablodev-2!' };
   }
 }
--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -8,6 +8,6 @@ import { Message } from '@pablodev2/api-interfaces';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
+  hello$ = this.http.get<Message>('/api');
   constructor(private http: HttpClient) {}
 }
