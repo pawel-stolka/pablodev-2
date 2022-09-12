879b1f0de5c55628bf37ce9f2b8a64e966b41abc
pabloDell <pawel.stolka@gmail.com>
2022-09-12 22:53:47 +0200

    może teraz?

--- libs/daily-learning/src/lib/containers/daily-learning-container/daily-learning-container.component.ts
+++ libs/daily-learning/src/lib/containers/daily-learning-container/daily-learning-container.component.ts
@@ -14,6 +14,10 @@ import {
   of,
   mergeAll,
   switchMap,
+  take,
+  timer,
+  flatMap,
+  reduce,
 } from 'rxjs';
 import { DailyLearningService } from '../../services/daily-learning.service';
 
@@ -28,6 +32,8 @@ export class DailyLearningContainerComponent implements OnInit {
   listItems$!: Observable<LearningItem[]>;
   groups$!: Observable<any[]>;
   example$!: Observable<any[]>;
+  categories2$!: Observable<LearningCategory[]>;
+  start$!: Observable<any[]>;
 
   constructor(
     private learningService: DailyLearningService,
@@ -35,6 +41,151 @@ export class DailyLearningContainerComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.serious4()
+    // this.rangle()
+  }
+
+  rangle() {
+    const setsOfValues = [
+      ["a", "b", "c", "d", "e", "f"],
+      [1, 2, 3, 4, 5, 6],
+      ["😀", "🐶", "🍏", "⚽️", "🚗", "⌚️"]
+    ];
+
+    const threeStreamsOfThings$ = timer(0, 1800).pipe(
+      take(3),
+      map(outerNumber =>
+        timer(0, 250).pipe(
+          take(6),
+          map(innerNumber => setsOfValues[outerNumber][innerNumber])
+        )
+      ),
+      // flatMap(value => value)
+      switchMap(value => value)
+    );
+
+    threeStreamsOfThings$.pipe(
+      tap(x => console.log('res', x))
+    ).subscribe()
+  }
+
+  serious4() {
+    const people = [
+      { name: 'Sue', age: 25 },
+      { name: 'Joe', age: 30 },
+      { name: 'Frank', age: 25 },
+      { name: 'Sarah', age: 35 }
+    ];
+    
+    this.start$ = from(people).pipe(
+      groupBy(person => person.age, p => p.name),
+      mergeMap(group => zip(of(group.key), group.pipe(toArray()))),
+      tap(([age, names]) => console.log('what', age, names)),
+      // map((res) => [{age: res[0], names: res[1]}]),
+      toArray(),
+      // map((res) => [{
+      //   a: res[0]
+      // }]),
+      tap(fin => console.log('fin', fin)),
+    )
+    // .subscribe(console.log);
+    
+  }
+
+  serious3() {
+    this.learnings$ = this.learningService.fetchAll().pipe(
+      tap(data => console.log('init data', data)),
+    )
+
+    const people = [  
+      { name: 'Alex', age: 31 },  
+      { name: 'Adam', age: 28 },  
+      { name: 'Alia', age: 21 },  
+      { name: 'David', age: 35 },  
+      { name: 'Rhea', age: 28 },  
+      { name: 'Samson', age: 31 },  
+      { name: 'Dhoni', age: 35 }  
+    ];  
+    let thisstart$ = from(people)  
+      .pipe(  
+        groupBy(  
+          person => person.age,  
+          p => p.name  
+        ),  
+        mergeMap(group => zip(of(group.key), group.pipe(toArray()))),
+        // map((a) => ({ a: a, b: a})),
+        flatMap(x => x),
+        // mergeAll(),
+        // tap(x => console.log(x))
+      )  
+      .subscribe(r => console.log('r', r));  
+    
+    // this.start$ = this.learnings$.pipe(
+    //   mergeMap(x => x),
+    //   groupBy(l => l.category),
+    //   mergeMap(group => group),
+    //   // switchMap(v => v.pipe(toArray())),
+    //   toArray(),
+    //   tap(data => console.log('fin', data)),
+    // )
+    // .subscribe()
+
+    
+  }
+
+  serious2() {
+    this.learnings$ = this.learningService.fetchAll().pipe(
+      tap(data => console.log('init data', data)),
+    )
+
+    // this.categories$
+    let thisstart$ = this.learnings$.pipe(
+      tap(data => console.log('first', data)),
+      mergeAll(),
+      groupBy(x => x.category),
+      // groupBy((x) => x.map(i => i.category)),
+      // flatMap(value => value),
+      switchMap(value => value.pipe(tap(x => console.log('?', x)))),//, x => x.pipe(toArray())),
+      toArray(),
+      tap(data => console.log('fin', data)),
+    ).subscribe()
+  }
+
+  serious() {
+    this.learnings$ = this.learningService.fetchAll().pipe(
+      tap(data => console.log('init data', data)),
+      
+    )
+    // this.categories$
+    this.start$ = this.learnings$.pipe(
+      map(items => items.map(item => ({ category: item.category}))),//, description: item.description  }))),
+      tap(res => console.log('res', res)),
+      groupBy((x) => x),
+      mergeMap(group => group),
+      tap(res => console.log('fin', res)),
+      // map(data => from(data)),
+      // mergeAll(),
+      // groupBy(item => item.category),
+      // mergeMap((group) => group.pipe(toArray())),
+      // map(items => items.map(x => ({
+      //   title: x.category,
+      // }))),
+      // toArray(),
+      // toArray(),
+      // tap(ex => console.log('examples grouped', ex))
+    )
+    // .subscribe(res => console.log('res grouped', res))
+  }
+
+  fromFetch(url: string): Observable<any> {
+    // : Observable<{userId: string, id: string, title: string, completed: boolean}[]> {
+    return this.http.get(url)//.pipe(map((res: any) => res.json()))
+    .pipe(
+      tap(raw => console.log('raw', raw))
+    )
+  }
+
+  demo() {
     // #1 demo
 
     // const source$ = items$.pipe(
@@ -44,28 +195,7 @@ export class DailyLearningContainerComponent implements OnInit {
     // source$.subscribe((x) => console.log('source$', x));
 
     // #2 real
-    const items$ = from([
-      {
-        id: 1,
-        title: 'uno',
-      },
-      {
-        id: 2,
-        title: 'dos',
-      },
-      {
-        id: 1,
-        title: 'tres',
-      },
-      {
-        id: 2,
-        title: 'quatro',
-      },
-      {
-        id: 2,
-        title: 'cinqo',
-      },
-    ]);
+    
     const fetchUrl = 'https://jsonplaceholder.typicode.com/todos';
     const todosFetch$ = this.fromFetch(fetchUrl)//.pipe(switchMap((res) => res.json()))
 
@@ -77,7 +207,6 @@ export class DailyLearningContainerComponent implements OnInit {
       toArray(),
       tap(ex => console.log('examples grouped', ex))
     );
-    // source$.subscribe((x) => console.log('source$', x));
 
     const learnings$ = this.learningService.fetchAll();
     // const g$ = learnings$.pipe(
@@ -116,13 +245,5 @@ export class DailyLearningContainerComponent implements OnInit {
     // console.log('groups2', groups)
     // console.log('thiscategories$', thiscategories$)
     this.learnings$ = learnings$;
-  }
-
-  fromFetch(url: string): Observable<any> {
-    // : Observable<{userId: string, id: string, title: string, completed: boolean}[]> {
-    return this.http.get(url)//.pipe(map((res: any) => res.json()))
-    .pipe(
-      tap(raw => console.log('raw', raw))
-    )
   }
 }
